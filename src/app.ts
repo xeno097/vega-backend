5d745d7e659676a1ba6e5613f07fc33fd5fc2c82
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-02-13 15:11:57 -0400

    added prettier and eslint files

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { errorHandlerMiddleware } from "./middlewares/error.middleware";
-import { NotFoundError } from "./errors/not-found.error";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { errorHandlerMiddleware } from './middlewares/error.middleware';
+import { NotFoundError } from './errors/not-found.error';
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   throw new NotFoundError();
 });
 
